e1719726a893e9968765c586389724e1fe81b305
Diego Armando Gomez Mosquera <dgomezcloud@outlook.com>
2020-12-21 11:30:19 -0500

    Return correct number of items

--- Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
+++ Tailspin.SpaceGame.Web/LocalDocumentDBRepository.cs
@@ -57,11 +57,11 @@ namespace TailSpin.SpaceGame.Web
             int page = 1, int pageSize = 10
         )
         {
-            var result = _items.AsQueryable()
+           var result = _items.AsQueryable()
                 .Where(queryPredicate) // filter
                 .OrderByDescending(orderDescendingPredicate) // sort
                 .Skip(page * pageSize) // find page
-                .Take(pageSize - 1) // take items
+                .Take(pageSize) // take items
                 .AsEnumerable(); // make enumeratable
 
             return Task<IEnumerable<T>>.FromResult(result);
